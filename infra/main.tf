eb066b170a32d3162f5b74ad80306326ef5c03a1
Veton Bejtullahu <vetonbejtullahu@Vetons-MacBook-Pro.local>
2025-10-26 23:48:38 +0100

    chore(tf): format Terraform files

--- infra/main.tf
+++ infra/main.tf
@@ -6,12 +6,12 @@ resource "azurerm_resource_group" "rg" {
 
 # Key Vault (name must be globally unique, alphanumeric and hyphens)
 resource "azurerm_key_vault" "kv" {
-  name                        = lower(replace("${var.project_name}-${var.env}-kv", "/[^a-zA-Z0-9-]/", ""))
-  location                    = var.location
-  resource_group_name         = azurerm_resource_group.rg.name
-  tenant_id                   = var.tenant_id
-  sku_name                    = "standard"
+  name                = lower(replace("${var.project_name}-${var.env}-kv", "/[^a-zA-Z0-9-]/", ""))
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+  tenant_id           = var.tenant_id
+  sku_name            = "standard"
 
-  purge_protection_enabled    = true
-  soft_delete_retention_days  = 7
+  purge_protection_enabled   = true
+  soft_delete_retention_days = 7
 }
