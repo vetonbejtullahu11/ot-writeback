630cda30c5d875aec1c055b08b281c62e9f4788c
Veton Bejtullahu <vetonbejtullahu@Vetons-MacBook-Pro.local>
2025-10-27 13:19:48 +0100

    chore: apply terraform fmt

--- infra/modules/monitor/main.tf
+++ infra/modules/monitor/main.tf
@@ -1,4 +1,4 @@
 # Dummy monitor module for CI
-output "law_id"                  { value = "dummy-law-id" }
-output "appi_id"                 { value = "dummy-appi-id" }
-output "appi_connection_string"  { value = "dummy-appi-connection-string" }
+output "law_id" { value = "dummy-law-id" }
+output "appi_id" { value = "dummy-appi-id" }
+output "appi_connection_string" { value = "dummy-appi-connection-string" }
--- infra/modules/sql/main.tf
+++ infra/modules/sql/main.tf
@@ -1,3 +1,3 @@
 # Dummy sql module for CI
 output "sql_server_fqdn" { value = "dummy-sql-server.database.windows.net" }
-output "database_name"   { value = "dummy-db-name" }
+output "database_name" { value = "dummy-db-name" }
